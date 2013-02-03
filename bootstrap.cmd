91f86050f742a4d839b01a62b2277089c45041f0
david@david-pc <david@davidjenni.com>
2013-02-02 19:24:04 -0800

    cleanup CRLF and tabs

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -9,10 +9,10 @@ set _HOME=%USERPROFILE%
 if "%_HOME%" == "" (echo ERROR: Cannot determine home directory for user %USERDOMAIN%\%USER% & exit /b 1)
 set dotPath=%_HOME%\dotfiles
 
-if /i "%1"=="setup" 		goto :doSetup
+if /i "%1"=="setup"         goto :doSetup
 if /i "%1"=="elevatedSetup" (shift && goto :elevatedSetup)
-if /i "%1"=="restore" 		goto :restoreAutorun
-if "%1" NEQ "" 				(echo ERROR: unknown verb: '%1'& exit /b 1)
+if /i "%1"=="restore"       goto :restoreAutorun
+if "%1" NEQ ""              (echo ERROR: unknown verb: '%1'& exit /b 1)
 
 if not exist "%ProgramFiles(x86)%\Git\bin\git.exe" (echo ERROR: requires Git to be installed, script needs git, curl and unzip&&exit /b 1)
 
@@ -22,61 +22,61 @@ if %ERRORLEVEL% EQU 1 goto :startSetup
 exit /b 4
 
 :startSetup
-	echo Cloning %dotPath% from origin repository: %githubDotfiles%
+    echo Cloning %dotPath% from origin repository: %githubDotfiles%
 echo .. call git clone %githubDotfiles% %dotPath%
-	if ERRORLEVEL 1 (echo ERROR cloning git repo %githubDotfiles% & exit /b 1)
+    if ERRORLEVEL 1 (echo ERROR cloning git repo %githubDotfiles% & exit /b 1)
 
-	:: to complete bootstrap, relaunch this script from freshly cloned repository
-	set this=%~nx0
-	call %dotPath%\%this% setup
-	exit /b 0
+    :: to complete bootstrap, relaunch this script from freshly cloned repository
+    set this=%~nx0
+    call %dotPath%\%this% setup
+    exit /b 0
 
 :doSetup
-	:: for each new cmd line window, hook in win32-rc (similar idea like .bashrc on unix)
-	set _win32rc=%dotPath%\win32-rc.cmd
-	if not exist "%_win32rc%" (echo ERROR cannot find %_win32rc%; is the dotfiles repository up-to-date?& exit /b 1)
+    :: for each new cmd line window, hook in win32-rc (similar idea like .bashrc on unix)
+    set _win32rc=%dotPath%\win32-rc.cmd
+    if not exist "%_win32rc%" (echo ERROR cannot find %_win32rc%; is the dotfiles repository up-to-date?& exit /b 1)
     reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "%_win32rc%" /f > nul
 
-	:: generate git secrets file:
-	echo.
-   	set _gitSecretsFile=%_HOME%\.gitSecrets.cmd
+    :: generate git secrets file:
+    echo.
+    set _gitSecretsFile=%_HOME%\.gitSecrets.cmd
     echo Enter email address to be used with Git (empty string will skip creating %_gitSecretsFile%).
     set /P _gitEmail=.  email: 
     if ERRORLEVEL 1 goto :setConsoleDefaults
-    	set _defaultGitUser=%USERNAME%@%COMPUTERNAME%
-	    echo Enter user name to be used with Git (default is %_defaultGitUser%).
-	    set /P _gitUser=.  name : || set _gitUser=%_defaultGitUser%
-		echo set GIT_AUTHOR_NAME=%_gitUser%> %_gitSecretsFile%
-		echo set GIT_AUTHOR_EMAIL=%_gitEmail%>> %_gitSecretsFile%
-		echo set GIT_COMMITTER_NAME=%%GIT_AUTHOR_NAME%%>> %_gitSecretsFile%
-		echo set GIT_COMMITTER_EMAIL=%%GIT_AUTHOR_EMAIL%%>> %_gitSecretsFile%
-		echo.
-		echo Git secrets file created: %_gitSecretsFile%
+        set _defaultGitUser=%USERNAME%@%COMPUTERNAME%
+        echo Enter user name to be used with Git (default is %_defaultGitUser%).
+        set /P _gitUser=.  name : || set _gitUser=%_defaultGitUser%
+        echo set GIT_AUTHOR_NAME=%_gitUser%> %_gitSecretsFile%
+        echo set GIT_AUTHOR_EMAIL=%_gitEmail%>> %_gitSecretsFile%
+        echo set GIT_COMMITTER_NAME=%%GIT_AUTHOR_NAME%%>> %_gitSecretsFile%
+        echo set GIT_COMMITTER_EMAIL=%%GIT_AUTHOR_EMAIL%%>> %_gitSecretsFile%
+        echo.
+        echo Git secrets file created: %_gitSecretsFile%
 
-	:setConsoleDefaults
-	    echo setting Console properties:
-	    set _consolePath=HKCU\Console
-	    reg add %_consolePath% /v QuickEdit         /d 0x1              /t REG_DWORD /f > nul
-	    reg add %_consolePath% /v WindowSize        /d 0x00320078       /t REG_DWORD /f > nul
-	    reg add %_consolePath% /v ScreenBufferSize  /d 0x23280078       /t REG_DWORD /f > nul
-	    reg add %_consolePath% /v FontFamily        /d 0x36             /t REG_DWORD /f > nul
+    :setConsoleDefaults
+        echo setting Console properties:
+        set _consolePath=HKCU\Console
+        reg add %_consolePath% /v QuickEdit         /d 0x1              /t REG_DWORD /f > nul
+        reg add %_consolePath% /v WindowSize        /d 0x00320078       /t REG_DWORD /f > nul
+        reg add %_consolePath% /v ScreenBufferSize  /d 0x23280078       /t REG_DWORD /f > nul
+        reg add %_consolePath% /v FontFamily        /d 0x36             /t REG_DWORD /f > nul
     	reg add %_consolePath% /v HistoryBufferSize /d 0x64             /t REG_DWORD /f > nul
-	    reg add %_consolePath% /v FaceName          /d "Consolas"       /t REG_SZ    /f > nul
-	    reg add %_consolePath% /v FontSize          /d 0x000E0000       /t REG_DWORD /f > nul
-	    set _consolePath=
+        reg add %_consolePath% /v FaceName          /d "Consolas"       /t REG_SZ    /f > nul
+        reg add %_consolePath% /v FontSize          /d 0x000E0000       /t REG_DWORD /f > nul
+        set _consolePath=
     echo.
     :: prime current cmd window
     call %_win32rc%
     echo Registry for current user has been updated.
 
-	:: Check for admin permissions
-	OPENFILES.exe > nul 2>&1
-	if ERRORLEVEL 1 (
-   		echo Requesting elevated privileges...
-   		call :requestUAC
-	) else (
-		call elevatedSetup %_HOME%
-	)
+    :: Check for admin permissions
+    OPENFILES.exe > nul 2>&1
+    if ERRORLEVEL 1 (
+        echo Requesting elevated privileges...
+        call :requestUAC
+    ) else (
+        call elevatedSetup %_HOME%
+    )
     exit /b 0
 
 :restoreAutorun
@@ -85,7 +85,7 @@ echo .. call git clone %githubDotfiles% %dotPath%
     reg add %_consolePath% /v QuickEdit         /d 0x0              /t REG_DWORD /f > nul
     reg add %_consolePath% /v WindowSize        /d 0x00190050       /t REG_DWORD /f > nul
     reg add %_consolePath% /v ScreenBufferSize  /d 0x012C0050       /t REG_DWORD /f > nul
-  	reg add %_consolePath% /v HistoryBufferSize /d 0x32             /t REG_DWORD /f > nul
+    reg add %_consolePath% /v HistoryBufferSize /d 0x32             /t REG_DWORD /f > nul
     reg add %_consolePath% /v FontFamily        /d 0x0              /t REG_DWORD /f > nul
     reg delete %_consolePath% /v FaceName                                        /f > nul 2>&1
     reg add %_consolePath% /v FontSize          /d 0x0              /t REG_DWORD /f > nul
@@ -95,25 +95,25 @@ echo .. call git clone %githubDotfiles% %dotPath%
     exit /b 0
 
 :requestUAC
-	set _uacVbsScript=%LocalAppData%\bootstrapWithUac.vbs
-   	echo set objShell = CreateObject^("Shell.Application"^) > %_uacVbsScript%
-   	echo objShell.ShellExecute "%~f0", "elevatedSetup %_HOME%", "", "runas", 1 >> %_uacVbsScript%
+    set _uacVbsScript=%LocalAppData%\bootstrapWithUac.vbs
+    echo set objShell = CreateObject^("Shell.Application"^) > %_uacVbsScript%
+    echo objShell.ShellExecute "%~f0", "elevatedSetup %_HOME%", "", "runas", 1 >> %_uacVbsScript%
 
     echo.
     echo Relaunching this script with elevation. Script will continue in a new window...
-   	cscript "%_uacVbsScript%" //nologo
-   	:: only for Win7 and newer!
-   	waitfor /t 20 elevatedSetupCompleted > nul
-   	if ERRORLEVEL 1 (
-   		echo ERROR: Bootstrapping is INCOMPLETE, likely due to a failure to elevate.
-	   	echo Bootstrapping can be resumed with:
-	   	echo     %~f0 setup
-	   	exit /b 1
- 	)
-   	echo.
-   	echo Bootstrapping complete.
-   	exit /B 0
-	goto :eof
+    cscript "%_uacVbsScript%" //nologo
+    :: only for Win7 and newer!
+    waitfor /t 20 elevatedSetupCompleted > nul
+    if ERRORLEVEL 1 (
+        echo ERROR: Bootstrapping is INCOMPLETE, likely due to a failure to elevate.
+        echo Bootstrapping can be resumed with:
+        echo     %~f0 setup
+        exit /b 1
+    )
+    echo.
+    echo Bootstrapping complete.
+    exit /B 0
+    goto :eof
 
 :elevatedSetup
     :: param1 home directory
@@ -128,11 +128,10 @@ echo .. call git clone %githubDotfiles% %dotPath%
 
     set dotFiles=gitconfig gitignore vimrc
 
-	echo .. ren %_HOME%\_vimrc %_HOME%\_vimrc.o
-    echo .. mklink %_HOME%\_vimrc %_HOME%\dotfiles\vimrc
-
-	echo .. ren %_HOME%\vimfiles %_HOME%\vimfiles.o
-	echo .. mklink /j %_HOME%\vimfiles %_HOME%\dotfiles\vimfiles
+echo .. ren %_HOME%\_vimrc %_HOME%\_vimrc.o
+echo .. mklink %_HOME%\_vimrc %_HOME%\dotfiles\vimrc
+echo .. ren %_HOME%\vimfiles %_HOME%\vimfiles.o
+echo .. mklink /j %_HOME%\vimfiles %_HOME%\dotfiles\vimfiles
 
     echo remap CapsLock to LeftCtrl key:
     REM see http://www.experts-exchange.com/OS/Microsoft_Operating_Systems/Windows/A_2155-Keyboard-Remapping-CAPSLOCK-to-Ctrl-and-Beyond.html
@@ -143,9 +142,8 @@ echo .. call git clone %githubDotfiles% %dotPath%
     echo.
     echo Bootstrapping with elevation complete.
 pause
-   	:: only for Win7 and newer!
-   	waitfor /si elevatedSetupCompleted
+    :: only for Win7 and newer!
+    waitfor /si elevatedSetupCompleted
     goto :eof
 
- 
 :eof
