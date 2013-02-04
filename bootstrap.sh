0824b71849bb4ecb485438b6cea2764887df7ad2
david@david-air <david@davidjenni.com>
2013-02-04 00:18:36 -0800

    bootstrap.sh aligned to Win version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,90 @@
 #!/bin/bash
 this="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 
+myGitHub='https://github.com/davidjenni/dotfiles.git'
+dotPath=$HOME/dotfiles
+
+
 createBackupDir() {
     local _thisTime=`date "+%y%m%d-%H%M%S"`
-    local _bkpDir=~/dotFiles.$_thisTime
+    local _bkpDir=$HOME/bootstrapBackups-$_thisTime
 
     mkdir $_bkpDir
     echo $_bkpDir
 }
 
 getRepository() {
-    pushd ~
+    pushd $HOME
     local repo=$1
     echo "Cloning git repo from: $repo"
     echo "      into local repo: $dotPath"
-    git clone $repo $dotPath
+   # git clone $repo $dotPath
     cd $dotPath
-    git submodule update --init
+   # git submodule update --init
     popd
 }
 
 saveLink() {
-    local orgFile=~/.$1
-    if [ -s "$orgFile" ]
+    local srcFile=$1
+    local targetFile=$2
+    if [ -s $targetFile ]
     then
-        echo "Saving $orgFile ..."
+        #echo "Saving $targetFile ..."
         # don't just move, do copy to break any previous soft link
-        cp $orgFile $bkpDir/$1
-        rm -f $orgFile
+        cp $targetFile $bkpDir/
+        echo rm -f $targetFile
     fi
-    ln $dotPath/$1 $orgFile
+    echo ln -s $srcFile $targetFile
+}
+
+function doSetup() {
+    # generate git secrets file:
+    local _gitSecretsFile=$HOME/.gitSecrets.sh
+    local _gitEmail
+    echo "Enter email address to be used with Git (empty string will skip creating $_gitSecretsFile)."
+    echo "   email: \c"
+    read _gitEmail
+    if [ "$_gitEmail" != "" ]; then
+        local _defaultGitUser=$USER@`hostname -s`
+        local _gitUser
+        echo "Enter user name to be used with Git (default is $_defaultGitUser)."
+        echo "    name: \c"
+        read _gitUser
+        if [ "$_gitUser" = "" ]; then
+            echo "defualt user"
+            _gitUser=$_defaultGitUser
+        fi
+
+        cat << EndOfSecrets > $_gitSecretsFile
+export GIT_AUTHOR_NAME='$_gitUser'
+export GIT_AUTHOR_EMAIL='$_gitEmail'
+export GIT_COMMITTER_NAME=\$GIT_AUTHOR_NAME
+export GIT_COMMITTER_EMAIL=\$GIT_AUTHOR_EMAIL
+EndOfSecrets
+
+        echo "Git secrets file created: $_gitSecretsFile"
+    fi
+
+    bkpDir=$(createBackupDir)
+    echo Original dot files will be saved away under: $bkpDir
+    echo
+
+    saveLink $dotPath/gitconfig $HOME/.gitconfig
+    saveLink $dotPath/hgrc $HOME/.hgrc
+    saveLink $dotPath/vimrc $HOME/.vimrc
+
+    saveLink $dotPath/bashrc $HOME/.bashrc
+    saveLink $dotPath/inputrc $HOME/.inputrc
+    saveLink $dotPath/pythonrc.py $HOME/.pythonrc.py
+    saveLink $dotPath/tmux.conf $HOME/.tmux.conf
 }
 
 # main:
-myGitHub='https://github.com/davidjenni/dotfiles.git'
-declare -a dotFiles=( bashrc gitconfig gitignore inputrc pythonrc.py tmux.conf vimrc )
-#dotPath=$(dirname $this)
-dotPath=~/dotfiles
-
-getRepository $myGitHub
-
-bkpDir=$(createBackupDir)
-echo Original dot files will be saved away under: $bkpDir
-echo
-
-for aFile in "${dotFiles[@]}"
-do
-    saveLink $aFile
-done
-
-cat << EndOfSecrets > ~/.secrets
-# Set your git user info in .secrets and source it from your shell startup:
-export GIT_AUTHOR_NAME='<add-your-name'
-export GIT_AUTHOR_EMAIL='<add-your-email>'
-export GIT_COMMITTER_NAME=$GIT_AUTHOR_NAME
-export GIT_COMMITTER_EMAIL=$GIT_COMMITTER_EMAIL
-EndOfSecrets
-
-echo Make sure to edit ~/.secrets and customize to your name and email for GIT
+if [ $1 = "setup" ]; then
+    doSetup
+else
+#    getRepository $myGitHub
+    echo do clean setup
+    doSetup
+fi
 
