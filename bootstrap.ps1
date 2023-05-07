c7a85c95a67c421394b066441be216d715657184
david@DAVID-PC <3200210+davidjenni@users.noreply.github.com>
2023-05-06 23:58:16 -0700

    Bootstrap in stages/verbs
    - introduce different stages/verbs
    - after cloning into local repo, contiue running bootstrap.ps1 from repo
      in child process
    - port cmd/console initialization into .ps1
    - prune obsolete info from doskey alias and win32-rc

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,13 +1,31 @@
-# to bootstrap, run this in a PowerShell prompt:
-# > Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-# > irm 'https://raw.githubusercontent.com/davidjenni/dotfiles/main/bootstrap.ps1' | iex
-#
+<#
+    .SYNOPSIS
+    Bootstrap Windows command prompts (cmd, PS, PSCore) with my dotfiles and apps.
+
+    .DESCRIPTION
+    to bootstrap directly from github, run these 2 cmdlets in a PowerShell prompt:
+    > Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+    > irm 'https://raw.githubusercontent.com/davidjenni/dotfiles/main/bootstrap.ps1' | iex
+#>
 [CmdletBinding()]
 param (
-    [Parameter()] [string] $userName = $env:USERNAME,
-    [Parameter()] [string] $computerName = $env:COMPUTERNAME,
-    [Parameter()] [string] $email = $null
+    [ValidateSet('clone', 'bootstrap', 'apps', 'env', IgnoreCase = $true)]
+    [Parameter(Position = 0)] [string]
+    # verb that indicates stage:
+    #  clone:       clone the dotfiles repo and continue with 'bootstrap' etc.
+    #  bootstrap:   setup PS, package managers, git. Includes 'apps' and 'env'.
+    #  apps:        install apps via winget and scoop
+    #  env:         setups consoles and configurations for git, neovim, PowerShell etc.
+    $verb = 'clone',
+    [Parameter()] [string]
+    # user name for git commits, defaults to '$env:USERNAME@$env:COMPUTERNAME'
+    $userName = $null,
+    [Parameter()] [string]
+    # email address for git commits, defaults to existing git config or prompts for input
+    $email = $null
 )
+
+$ErrorActionPreference = 'Stop'
 
 $originGitHub='https://github.com/davidjenni/dotfiles.git'
 $dotPath=(Join-Path $env:USERPROFILE 'dotfiles')
@@ -16,80 +34,14 @@ $dotPath=(Join-Path $env:USERPROFILE 'dotfiles')
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor
                                               [Net.SecurityProtocolType]::Tls12
 
-function modernizeWinPowerShell {
-    if ($PSVersionTable.PSEdition -eq "Core") {
-        Write-Host "Core edition detected, skipping modernization."
-        return
-    }
-    Write-Host "Modernizing Desktop PowerShell $($PSVersionTable.PSVersion)..."
-
-    Write-Host " - Installing NuGet provider..."
-    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser | Out-Null
-    Register-PackageSource -Provider NuGet -Name nugetRepository -Location https://www.nuget.org/api/v2 -Force -Trusted | Out-Null
-
-    function upgradeModule {
-        param (
-            [string] $name,
-            [Version] $minVersion
-        )
-        Write-Host -NoNewline " - Checking $($name.PadRight(40))"
-        $info = Import-Module -Name $name -PassThru -ErrorAction SilentlyContinue
-        if ($null -ne $info -and $info.Version -ge $minVersion) {
-            Write-Host "(up-to-date: $minVersion)"
-            return
-        }
-        Write-Host -NoNewline "upgrading to $minVersion"
-        Remove-Module -Name $name -Force -ErrorAction SilentlyContinue
-        Install-Module -Name $name -MinimumVersion $minVersion -AllowClobber -Force -Scope CurrentUser
-        Import-Module -Name $name -Force
-        Write-Host "; done."
-    }
-
-    # upgradeModule 'PackageManagement' '1.4.8.1'
-
-    Write-Host " - PSGallery:"
-    $psgallery = Get-PSRepository -Name PSGallery -ErrorAction SilentlyContinue
-    if ($null -eq $psgallery -or $psgallery.InstallationPolicy -ne 'Trusted') {
-        Write-Host "   - Registering PSGallery..."
-        Register-PSRepository -Default -ErrorAction SilentlyContinue
-        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    }
-
-    upgradeModule 'PowerShellGet' '2.2.5'
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-    upgradeModule 'PSReadLine' '2.2.6'
-}
-
-function ensureWinGet {
-    $wget = Get-Command "winget.exe" -ErrorAction SilentlyContinue
-    if ($null -ne $wget) {
-        Write-Host "winget already installed."
-        return
-    }
-    Write-Host "Installing winget..."
-
-    # https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages
-    $vc14 = (Join-Path $env:TEMP "vc14.appx")
-    (New-Object Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', $vc14)
-    Add-AppxPackage -Path $vc14
-
-    $uixaml = Install-Package Microsoft.UI.Xaml -scope currentuser -RequiredVersion 2.7.3 -force
-    Add-AppxPackage -Path $env:LOCALAPPDATA\PackageManagement\nuget\Packages\Microsoft.UI.Xaml.$($uixaml.Version)\tools\AppX\x64\Release\Microsoft.UI.Xaml.*.appx
-
-    # https://github.com/microsoft/winget-cli/releases
-    Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.5.441-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-}
-
 function ensureLocalGit {
-    $git = Get-Command 'git' -ErrorAction SilentlyContinue
-    if ($null -ne $git) {
+    if (Get-Command 'git' -ErrorAction SilentlyContinue) {
         return
     }
 
     # bootstrap with a local git to avoid early elevating for winget and the git installer:
     $localGitFolder = (Join-Path $env:USERPROFILE (Join-Path "Downloads" "localGit"))
-    Write-Host "Installing local git into $localGitFolder..."
+    Write-Host "Installing ad-hoc git into $localGitFolder..."
 
     $gitUrl = Invoke-RestMethod 'https://api.github.com/repos/git-for-windows/git/releases/latest' |
         Select-Object -ExpandProperty 'assets' |
@@ -105,6 +57,130 @@ function ensureLocalGit {
     $env:Path += ";$gitPath"
 }
 
+function cloneDotfiles {
+    Write-Host "Cloning $originGitHub -> $dotPath"
+    Write-Host -NoNewline "OK to proceed with setup? [Y/n] "
+    $answer = (Read-Host).ToUpper()
+    if ($answer -ne 'Y' -and $answer -ne '') {
+        Write-Warning "Aborting."
+        return 4
+    }
+
+    ensureLocalGit
+
+    if (-not $userName -or $userName -eq '') {
+        $userName = (& git config --global --get user.name)
+    }
+    if (-not $username -or $username -eq '') {
+        $username = "$env:USERNAME@$env:COMPUTERNAME"
+    }
+
+    if (-not $email -or $email -eq '') {
+        $email = (& git config --global --get user.email)
+    }
+    if (-not $email -or $email -eq '') {
+        $email = Read-Host "Enter your email address for git commits"
+        if ($email -eq '') {
+            Write-Warning "Need email address, aborting."
+            return 3
+        }
+    }
+
+    & git.exe config --global user.name $userName
+    # https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-personal-account-on-github/managing-email-preferences/setting-your-commit-email-address
+    & git.exe config --global user.email $email
+
+    & git clone $originGitHub $dotPath
+    return 0
+}
+
+function modernizeWinPowerShell {
+    if ($PSVersionTable.PSEdition -eq "Core") {
+        Write-Host "Core edition detected, skipping modernization."
+        return
+    }
+    Write-Host "Updating package mgmt for stock PowerShell $($PSVersionTable.PSVersion)..."
+
+    if (-not (Get-PackageSource -ProviderName nuget -Force -ErrorAction SilentlyContinue)) {
+        Write-Host " - Installing NuGet provider..."
+        Install-PackageProvider -Name NuGet -Force -Scope CurrentUser | Out-Null
+        Register-PackageSource -Provider NuGet -Name nugetRepository -Location https://www.nuget.org/api/v2 -Force -Trusted | Out-Null
+    }
+
+    function upgradeModule {
+        param (
+            [string] $name,
+            [Version] $minVersion
+        )
+        Write-Host -NoNewline " - Checking $($name.PadRight(40))"
+        $info = Import-Module -Name $name -PassThru -ErrorAction SilentlyContinue
+        if ($info -and $info.Version -ge $minVersion) {
+            Write-Host "(up-to-date: $minVersion)"
+            return
+        }
+        Write-Host -NoNewline "upgrading to $minVersion"
+        Remove-Module -Name $name -Force -ErrorAction SilentlyContinue
+        Install-Module -Name $name -MinimumVersion $minVersion -AllowClobber -Force -Scope CurrentUser
+        Import-Module -Name $name -Force
+        Write-Host "; done."
+    }
+
+    # upgradeModule 'PackageManagement' '1.4.8.1'
+
+    Write-Host " - PSGallery:"
+    $psgallery = Get-PSRepository -Name PSGallery -ErrorAction SilentlyContinue
+    if (-not $psgallery -or $psgallery.InstallationPolicy -ne 'Trusted') {
+        Write-Host "   - Registering PSGallery..."
+        Register-PSRepository -Default -ErrorAction SilentlyContinue
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    }
+
+    upgradeModule 'PowerShellGet' '2.2.5'
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+    upgradeModule 'PSReadLine' '2.2.6'
+}
+
+function ensureWinGet {
+    if (Get-Command "winget.exe" -ErrorAction SilentlyContinue) {
+        Write-Host "winget already installed."
+        return
+    }
+    if ($PSVersionTable.PSEdition -ne "Desktop") {
+        Write-Host "Installing winget requires Desktop PowerShell, aborting."
+        return
+    }
+
+    Write-Host "Installing winget..."
+    # https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge#how-to-install-and-update-desktop-framework-packages
+    $vc14 = (Join-Path $env:TEMP "vc14.appx")
+    (New-Object Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', $vc14)
+    Add-AppxPackage -Path $vc14
+
+    $uixaml = Install-Package Microsoft.UI.Xaml -scope currentuser -RequiredVersion 2.7.3 -force
+    Add-AppxPackage -Path $env:LOCALAPPDATA\PackageManagement\nuget\Packages\Microsoft.UI.Xaml.$($uixaml.Version)\tools\AppX\x64\Release\Microsoft.UI.Xaml.*.appx
+
+    # https://github.com/microsoft/winget-cli/releases
+    Add-AppxPackage -Path https://github.com/microsoft/winget-cli/releases/download/v1.5.441-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+}
+
+function ensureScoop {
+    if (Get-Command "scoop" -ErrorAction SilentlyContinue) {
+        Write-Host "scoop already installed."
+        return
+    }
+
+    # https://scoop.sh/
+    Write-Host "Installing scoop..."
+    Invoke-RestMethod 'get.scoop.sh' | Invoke-Expression
+    # TODO: check if elevated and run as admin if needed:
+    # https://stackoverflow.com/questions/60209449/how-to-elevate-a-powershell-script-from-within-a-script
+    # by default, scoop does not like to run elevated:
+    # Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
+    $scoopPath = (Join-Path $env:USERPROFILE (Join-Path 'scoop' 'shims'))
+    $env:Path += ";$scoopPath"
+}
+
 function mklink {
     param (
         [Parameter(Mandatory = $true)] [string] $target,
@@ -118,16 +194,6 @@ function mklink {
 }
 
 function installScoopApps {
-    $sc = Get-Command "scoop" -ErrorAction SilentlyContinue
-    if ($null -eq $sc) {
-        # https://scoop.sh/
-        Write-Host "Installing scoop..."
-        Invoke-RestMethod 'get.scoop.sh' | Invoke-Expression
-        # by default, scoop does not like to run elevated:
-        # Invoke-Expression "& {$(Invoke-RestMethod get.scoop.sh)} -RunAsAdmin"
-        $scoopPath = (Join-Path $env:USERPROFILE (Join-Path 'scoop' 'shims'))
-        $env:Path += ";$scoopPath"
-    }
     Write-Host "Installing apps via scoop..."
     & scoop install 7zip bat delta fd fzf helix less lsd neovim nuget nvm ripgrep starship tre-command
     & scoop bucket add nerd-fonts
@@ -143,50 +209,97 @@ function installWinGetApps {
     & winget install Git.Git --accept-source-agreements --disable-interactivity
     # TODO: need to figure out how this interacts with a soft-linked gitconfig from this repo
     # TODO: one option: loop over simple git setting file and make 'git config --global key value' calls
-    & git.exe config --global user.name $userName@$computerName
-    & git.exe config --global user.email $email
 
     # requires elevation:
     & winget install Microsoft.Powershell --accept-source-agreements --disable-interactivity
 
     # per user, no elevation required:
-    # $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
     & winget install Microsoft.WindowsTerminal --accept-source-agreements --disable-interactivity
     & winget install Microsoft.VisualStudioCode --accept-source-agreements --disable-interactivity
 }
 
-function main {
-    Write-Host
-    Write-Host "Bootstrap $originGitHub -> $dotPath"
-    Write-Host -NoNewline "OK to proceed with setup? [Y/n] "
-    $answer = (Read-Host).ToUpper()
-    if ($answer -ne 'Y' -and $answer -ne '') {
-        Write-Warning "Aborting."
-        return
-    }
-
-    if (($null -eq $email -or $email -eq '') -and $null -ne (Get-Command 'git' -ErrorAction SilentlyContinue)) {
-        $email = (& git config --global --get user.email)
-    }
-    if ($null -eq $email -or $email -eq '') {
-        $email = Read-Host "Enter your email address for git commits"
-        if ($email -eq '') {
-            Write-Warning "Need email address, aborting."
-            return
-        }
-    }
-
+function bootstrap {
     modernizeWinPowerShell
     ensureWinGet
+    ensureScoop
+}
 
-    ensureLocalGit
-    & git clone $originGitHub $dotPath
-
-    # TODO: relaunch this script from repo
-    # TODO: either symlink or copy gitconfig, profile.ps1, init.lua etc to $env:USERPROFILE
-
+function installApps {
     installScoopApps
     installWinGetApps
 }
 
-main
+function setupShellEnvs {
+    Write-Host "setting cmd console properties:"
+    $consolePath='HKCU\Console'
+    & reg add $consolePath /v QuickEdit         /d 0x1              /t REG_DWORD /f | Out-Null
+    & reg add $consolePath /v WindowSize        /d 0x00320078       /t REG_DWORD /f | Out-Null
+    & reg add $consolePath /v ScreenBufferSize  /d 0x23280078       /t REG_DWORD /f | Out-Null
+    & reg add $consolePath /v FontFamily        /d 0x36             /t REG_DWORD /f | Out-Null
+    & reg add $consolePath /v HistoryBufferSize /d 0x64             /t REG_DWORD /f | Out-Null
+    & reg add $consolePath /v FaceName          /d "Hack Nerd Font Mono" /t REG_SZ  /f | Out-Null
+    & reg add $consolePath /v FontSize          /d 0x00100000       /t REG_DWORD /f | Out-Null
+
+    $win32rc=(Join-Path $dotPath (Join-Path 'win' 'win32-rc.cmd'))
+    Write-Host "setting up cmd autorun: $win32rc"
+    & reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d $win32rc /f | Out-Null
+
+    # TODO: needs elevation
+    # Write-Host "remap CapsLock to LeftCtrl key:"
+    # # see http://www.experts-exchange.com/OS/Microsoft_Operating_Systems/Windows/A_2155-Keyboard-Remapping-CAPSLOCK-to-Ctrl-and-Beyond.html
+    # # http://msdn.microsoft.com/en-us/windows/hardware/gg463447.aspx
+    # & reg add "HKLM\SYSTEM\CurrentControlSet\Control\Keyboard Layout" /v "Scancode Map" /d 0000000000000000020000001D003A0000000000 /t REG_BINARY /f | Out-Null
+    # Write-Host "CapsLock remapped, will be effective after next system reboot."
+
+    # TODO: initialize Terminal, but its .json file won't exist until after the first launch
+    # $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+}
+
+function main {
+    param (
+        [Parameter(Mandatory = $true)] [string] $verbAction
+    )
+
+    Write-Verbose "PS: $($PSVersionTable.PSVersion)-$($PSVersionTable.PSEdition)"
+    switch ($verbAction) {
+        'clone' {
+            Write-Host
+            if (Test-Path (Join-Path $dotPath '.git')) {
+                Write-Host "local git repo already exists, skipping."
+                # continue in-proc:
+                main bootstrap
+                return
+            }
+
+            $rc = cloneDotfiles
+            if ($rc -ne 0) {
+                Write-Error "Cloning dotfiles failed, aborting."
+                return
+            }
+            # continue with now-local bootstrap.ps1 from cloned repo:
+            # still stick with desktop PS since PSCore is not necessarily installed yet
+            $script= (Join-Path $dotPath 'bootstrap.ps1')
+            Write-Host "Continue $script in child process"
+            Start-Process -PassThru -NoNewWindow -FilePath "powershell.exe" -ArgumentList "-NoProfile -File $script bootstrap" |
+                Wait-Process
+        }
+
+        'bootstrap' {
+            Write-Host "Bootstrapping..."
+            bootstrap
+            installApps
+            setupShellEnvs
+            Write-Host "Done (bootstrap)."
+            exit
+        }
+
+        'apps' { installApps }
+
+        'env' { setupShellEnvs }
+    }
+
+    # TODO: either symlink or copy gitconfig, profile.ps1, init.lua etc to $env:USERPROFILE
+    Write-Host "Done."
+}
+
+main $verb
--- win/win32-rc.cmd
+++ win/win32-rc.cmd
@@ -13,41 +13,13 @@ set _HERE=
 set HOME=%USERPROFILE%
 set MY_BIN=%HOME%
 
-call :addToPath "%MY_BIN%\dotfiles\win"
-call :addToPath "%USERPROFILE%\SkyDrive\Bin\Win"
-call :addToPath "%MY_BIN%\PuTTY"
-if exist "%ProgramFiles%\vim\vim80" (
-    call :addToPath "%ProgramFiles%\vim\vim80"
-) else (
-    if exist "%ProgramFiles%\vim\vim74" (
-        call :addToPath "%ProgramFiles%\vim\vim74"
-    ) else (
-        call :addToPath "%MY_BIN%\vim\vim74"
-    )
-)
-:: install emacs from: http://emacsbinw64.sourceforge.net/
-call :addToPath "%MY_BIN%\emacs\bin"
-call :addToPath "%ProgramFiles%\TortoiseHg"
-call :addToPath "%ProgramFiles(x86)%\Git\bin"
-:: Git for windows >= 2.6 installs as x64
-call :addToPath "%ProgramFiles%\Git\bin"
 call :addToPath "%ProgramFiles%\Git\usr\bin"
-call :addToPath "%ProgramFiles%\7-Zip"
-call :addToPath "%MY_BIN%\NTTools"
-call :addToPath "%MY_BIN%\ruby-1.9.1\bin"
-call :addToPath "%MY_BIN%\node_modules\.bin"
-call :addToPath "%APPDATA%\Python\Python35\Scripts"
 
 :setEnv
 set LESS=-i -M -N -q -x4 -R
 set LESSBINFMT=*d[%02x]
 
-set _gitSecrets=%HOME%\.gitSecrets.cmd
-if exist "%_gitSecrets%" call "%_gitSecrets%"
-set _gitSecrets=
-:: setup putty/plink to work properly with git: remotes
-if exist %MY_BIN%\putty set GIT_SSH=%MY_BIN%\putty\plink.exe
-
+set EDITOR=code --wait
 set VISUAL=code --wait
 
 set DIRCMD=/ogen
@@ -61,8 +33,8 @@ if /i "%PROCESSOR_ARCHITECTURE%"=="amd64" (
 
 if exist %MY_BIN%\go (
     set GOPATH=%MY_BIN%\go
+    call :addToPath "%MY_BIN%\go\bin"
 )
-call :addToPath "%MY_BIN%\go\bin"
 
 REM set prompt to the following:
 REM (20:51:41.60) [D:\]
