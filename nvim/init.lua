5a7838954f925a5852442b9b696b0bd379e2f961
David JENNI <3200210+davidjenni@users.noreply.github.com>
2025-03-29 15:59:03 -0700

    Neovim 0.11 (#63)
    * neovim 0.11.0: adopt its new, integrated LSP config
    
    - replace obsoleted neodev.nvim with lazydev.lua
    
    * use nvim --clean as git editor -> fast startup
    
    update stale doskey aliases to use eza

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,20 @@
 -- neovim standard paths:
 -- https://neovim.io/doc/user/starting.html#standard-path
 
+if vim.fn.has("nvim-0.11.0") == 0 then
+  vim.api.nvim_echo({
+    { "This nvim config requires neovim >= 0.11.0\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
+end
+
 require('options')
 require('keymaps')
 require('autocmds')
 require('lazy-nvim')
+require('lsp-config')
 
 vim.cmd[[ colorscheme catppuccin-macchiato ]]
